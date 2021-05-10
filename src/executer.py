5161f88c1cbc8c78358b44097e57d7fab4f4d376
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-10 14:33:53 +0430

    betta version complete

--- src/executer.py
+++ src/executer.py
@@ -1,13 +1,50 @@
+from functools import wraps
+import time
+from random import choice
+
+def time_protect(time):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(self, msg, *args, **kwargs):
+            print ("LOG [time_protect]:",msg)
+            return fn(self, msg, *args, **kwargs)
+        return wrapper
+    return decorator
+
 class Executer:
     def __init__(self, bot):
         self.bot = bot
+        self.nums = [x for x in range(1, 10)]
+        self.oper = ["*", "+", "-"]
 
-    def start(self):
-        # start the game
-        # generate 3 random numbers
-        # generate 2 random operators
-        # send the message
-        # get the response back
-        # check the answer
-        # check the response time
-        # gameover or repeat
+    def start_game(self, message):
+        self.bot.send_text(message["from"],
+            "شما فقط 5 ثانیه برای پاسخ دادن وقت دارید"
+        )
+        self.start(message)
+
+    @time_protect(60*60*24)
+    def start(self, message):
+        response = self.get_random_math_problem()
+        self.answer = eval(response)
+        self.bot.send_text(message["from"], response)
+        self.start_time = time.perf_counter()
+
+    def process_response(self, response):
+        self.elapsed = time.perf_counter() - self.start_time
+        if int(response["body"]) == self.answer:
+            print ("answer was true")
+            self.start(response)
+            return
+        
+        self.bot.send_text(
+            response["from"],
+            'بازی را باختید، جواب مورد انتظار={}'.format(self.answer)
+        )
+
+    def get_random_math_problem(self):
+        n = choice([3,5])
+        return " ".join([
+            str(choice(self.nums)) if x%2==1 else choice(self.oper)
+            for x in range(1, n+1)
+        ])
--- src/main.py
+++ src/main.py
@@ -1,17 +1,26 @@
 from client import Client 
 from token_loader import load_token
+from executer import Executer
+from threading import Thread
+
+
+def message_loop(messages_iter, bot):
+    executer = Executer(bot)
+    for msg in messages_iter:
+        if msg["body"] == "start":
+            executer.start_game(msg)
+        elif msg["body"].lstrip("-+").isdigit():
+            executer.process_response(msg)
 
 def main():
     token = load_token()
     bot = Client(token)
+
     try:
         messages = bot.get_messages()
-        for message in messages:
-            print (message)
-            message["to"] = message["from"]
-            message.pop("from")
-            message.pop("time")
-            bot.send_message(message)
+        message_loop(messages, bot)
+    
+
     except KeyboardInterrupt:
         exit()
     except Exception as e:
