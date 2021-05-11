4d97d2f2dbe239af472cc40065f34f7e468bce71
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-11 12:20:00 +0430

    update: logger.py
    details:
    - add date and time to logs
    - add `log_on_call` decorator
    - fixed: return value prefixed with `cls` in `Logger` class

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,5 @@
-from functools import partial
+from functools import partial, wraps
+import datetime
 
 class Logger:
     instance = None
@@ -6,7 +7,7 @@ class Logger:
     def __new__(cls, *args, **kwargs):
         if not cls.instance:
             return super().__new__(cls)
-        return instance
+        return cls.instance
 
     def __init__(self, log_file, log_to_console = True):
         self.log_file = log_file
@@ -16,10 +17,18 @@ class Logger:
         self.error = partial(self.__log, "warning")
 
     def __log(self, type: str, msg: str):
-        log_msg = f"[{type.upper()}] {msg}"
+        log_msg = f"[{type.upper()}] date/time:{datetime.datetime.now()} {msg}"
         if self.log_to_console:
             print (log_msg)
         self.log_file.write("{}\n".format(log_msg))
+        self.log_file.flush()
 
     def __del__(self):
         self.log_file.close()
+
+def log_on_call(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        Logger(open("../log/log.txt", "a+")).log(f"call to function: {fn.__name__}| ({args}, {kwargs})")
+        return fn(*args, **kwargs)
+    return wrapper
