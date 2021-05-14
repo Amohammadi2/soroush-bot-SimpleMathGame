5e0576c910831505c8090a23385401bdc7b9df06
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-14 15:34:19 +0430

    feature: time limit implemented
    details:
    - add `check_fn_exists` function
    - replace if statements in wrappes with `@check_fn_exists` decorators

--- src/guards.py
+++ src/guards.py
@@ -8,9 +8,16 @@ BANNED = []
 LOG_FILE = open("../log/log.txt", "a+", encoding="utf-8")
 LOGGER = Logger(LOG_FILE)
 
-def max_message_len(length):
-    def decorator(fn):
+def check_fn_exists(dec):
+    @wraps(dec)
+    def wrapper(fn):
         if not fn: return
+        return dec(fn)
+    return wrapper
+
+def max_message_len(length):
+    @check_fn_exists
+    def decorator(fn):
         def wrapper(bot, msg, *args, **kwargs):
             LOGGER.log(msg)
             LOGGER.log(f"GB users: {GOING_TO_BE_BANNED}")
@@ -41,8 +48,8 @@ def max_message_len(length):
 
 
 def allow_message_types(type_list: list):
+    @check_fn_exists
     def decorator(fn):
-        if not fn: return
         @wraps(fn)
         def wrapper(bot, msg, *args, **kwargs):
             if msg["type"] in type_list: return fn(bot, msg, *args, **kwargs)
@@ -51,8 +58,8 @@ def allow_message_types(type_list: list):
 
 
 def noexcept(excep_type):
+    @check_fn_exists
     def decorator(fn):
-        if not fn: return
         @wraps(fn)
         def wrapper(*args, **kwargs):
             with suppress(excep_type):
@@ -60,11 +67,17 @@ def noexcept(excep_type):
         return wrapper
     return decorator
 
-def time_limit(time):
+def time_limit(_time):
+    @check_fn_exists
     def decorator(fn):
         @wraps(fn)
         def wrapper(bot, msg, *args, **kwargs):
-            print (msg["time"])
+            m_time = int(msg["time"]) / 1000 # convert to seconds
+            if (exp_bool:=(exp:=time.time() - m_time) > _time):
+                print ("time limit hit")
+                return
+            print (exp, exp_bool)
+            print (_time)
             return fn(bot, msg, *args, **kwargs)
         return wrapper
     return decorator
